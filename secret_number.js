0e78b7a3de9d18b9fa7367f444ed57e95e8c7e83
jeffmon <jeffmon@gmail.com>
2017-05-18 19:11:35 -1000

    finished exercise

--- secret_number.js
+++ secret_number.js
@@ -1,4 +1,20 @@
 'use strict';
 module.exports = function() {
-  
+  var secretNumber = Math.floor(Math.random() * 10000);
+  return function(){
+    return secretNumber;
+  };
 };
+
+
+/*
+  var secretNumberGenerator = (function(){
+  var number = 0;
+  return function(){
+    number = Math.floor(Math.random() * 1000000) + 1;
+    return number;
+  }
+  })();
+  var secretNumber;
+  secretNumber = secretNumberGenerator();*/
+
